015d05d27dbcfc8e11ba160c7bf696bd5d1c35b8
KobbyKnight <isaacson878@gmail.com>
2019-05-21 18:19:39 +0000

    fixed upload

--- dbconnection.php
+++ dbconnection.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER','localhost');
 define('DB_USER','root');
-define('DB_PASS' ,'snithy123');
+define('DB_PASS' ,'');
 define('DB_NAME', 'loginsystem');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 
@@ -12,4 +12,3 @@ echo "Failed to connect to MySQL: " . mysqli_connect_error();
  }
 
 ?>
-
